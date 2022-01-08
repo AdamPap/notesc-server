cff75cd3b577c1dd80a72e159b088e79af429ef1
Adam <adampapamast@hotmail.com>
2022-01-08 15:41:41 +0200

    add: basic CRUD operations for Card

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,102 @@
 import "reflect-metadata";
+import "dotenv-safe/config";
 import express from "express";
 import { createConnection, Connection } from "typeorm";
+import { Card } from "./entities/Card";
+import path from "path";
 
 const main = async () => {
   const app = express();
 
+  app.use(express.json());
+
   try {
-    const connection: Connection = await createConnection();
+    const connection: Connection = await createConnection({
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      logging: true,
+      migrationsRun: true,
+      entities: [Card],
+      migrations: [path.join(__dirname, "./migrations/*")],
+    });
 
     if (connection.isConnected) {
       console.log("DB Connection successful.");
+      // connection.runMigrations();
+      // console.log("DB Migrated successfully.");
     }
   } catch (err) {
-    console.log("DB Connection error: ", err);
+    console.error("DB Connection error: ", err);
   }
 
   app.get("/", (_, res) => {
     res.send("INDEX");
+  });
+
+  app.get("/cards", async (_, res) => {
+    try {
+      const cards = await Card.find();
+
+      console.log(cards);
+
+      res.json(cards);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  app.post("/cards", async (req, res) => {
+    console.log(req.body);
+
+    const { title, content } = req.body;
+
+    const card = await Card.create({ title, content });
+    await card.save();
+
+    console.log(card);
+
+    res.status(201).json(card);
+  });
+
+  app.put("/cards/:cardId", async (req, res) => {
+    const cardId = parseInt(req.params.cardId);
+
+    const card = await Card.findOne({ id: cardId });
+
+    if (!card) {
+      throw new Error("Card not found");
+    }
+
+    const { title, content } = req.body;
+
+    Card.update(
+      { id: cardId },
+      {
+        title,
+        content,
+      }
+    );
+
+    // card.title = title;
+    // card.content = content;
+
+    // card.save();
+
+    res.json(card);
+  });
+
+  app.delete("/cards/:cardId", async (req, res) => {
+    const cardId = parseInt(req.params.cardId);
+
+    const card = Card.findOne({ id: cardId });
+
+    if (!card) {
+      throw new Error("Card not found");
+    }
+
+    await Card.delete({ id: cardId });
+
+    res.json({ deleted: true });
   });
 
   const port = process.env.PORT;
@@ -25,4 +105,6 @@ const main = async () => {
   });
 };
 
-main();
+main().catch((err) => {
+  console.error(err);
+});
